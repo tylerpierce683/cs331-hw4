57dc1f25d0fb000bdda959e86807951e76ec0e37
Tyler Pierce <tylerpierce683@u.boisestate.edu>
2025-04-02 13:16:36 -0600

    Modified access of event_data

--- part2.py
+++ part2.py
@@ -12,7 +12,7 @@ INTEGRITY = {"Low": 1, "Medium": 2, "High": 3}
 # Security log Function
 def add_log_event(user_id, event_data):
     with open("security_log.txt", "a") as log:
-        log.write(f"User: {user_id}, File: {event_data.file}, Action: {event_data.action}, Status:{event_data.status}\n")
+        log.write(f"User: {user_id}, File: {event_data["file"]}, Action: {event_data["action"]}, Status:{event_data["status"]}\n")
     
 # Security Check Function
 def check_access(user_id, file, action):
